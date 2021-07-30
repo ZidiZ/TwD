64257fce40d762a0aba46d416e6cf13c2f04bf0d
ZidiZ <2545266z@student.gla.ac.uk>
2021-07-30 12:12:25 +0100

    chapter 6 test

--- rango/urls.py
+++ rango/urls.py
@@ -4,13 +4,12 @@ from rango import views
 app_name = 'rango'
 
 urlpatterns = [
-	path('', views.index, name='index'),
-	
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('category/<slug:category_name_slug>/',
+         views.show_category, name='show_category'),
 ]
 
-urlpatterns = [
-	path('about/', views.about, name='about'),
-]
 
 #urlpatterns = [
 #	path('rango/', views.index, name='index'),
